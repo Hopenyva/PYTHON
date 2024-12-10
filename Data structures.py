fd671e061f41e004165d5ae7005ebcef7ebb9907
Hopenyva <nyvahhope@gmail.com>
2024-12-10 18:19:13 +0300

    Data structures

---
+++ Data structures.py
@@ -0,0 +1,33 @@
+my_list = []
+print(my_list)
+my_list.append(10)
+print("Before Append:", my_list)
+
+
+my_list.append(20)
+print("After Append:", my_list)
+print("Before Append:", my_list)
+my_list.append(30)
+print("After Append:", my_list)
+print("Before Append:", my_list)
+my_list.append(40)
+print("After Append:", my_list)
+my_list.insert(2,15)
+print(my_list)
+my_list.extend([50, 60, 70])
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+index = my_list.index(30)
+print("The index of 30 is:" , index)
+
+
+
+
+
+
+
+
+
